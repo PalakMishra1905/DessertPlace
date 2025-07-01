8ee19386f6fc762b358c1936573fb986b50c73cf
Palak Mishra <mishrapalak104@gmail.com>
2025-07-01 11:39:26 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 const StoreContextProvider = (props)=>{
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://dessertplace-backend.onrender.com";
+    const url = "https://dessertplace-backend-1.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
